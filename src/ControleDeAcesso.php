a8a344f76aed1963ad3a43914175497925e27b67
André Marques <andre.m.d.abreu@gmail.com>
2023-10-17 09:27:11 -0300

    Método login

--- src/ControleDeAcesso.php
+++ src/ControleDeAcesso.php
@@ -24,4 +24,14 @@ final class ControleDeAcesso {
             die(); // ou exit;
         }
     }
+
+    public function login(int $id, string $nome, string $tipo):void {
+        /* No momento em que ocorre login, criamos variáveis
+        de sessão contendo os dados que queremos monitorar/controlar/usar
+        através da sessão enquanto a pessoa estiver logada. */
+        $_SESSION["id"] = $id;
+        $_SESSION["nome"] = $nome;
+        $_SESSION["tipo"] = $tipo;
+
+    }
 }
