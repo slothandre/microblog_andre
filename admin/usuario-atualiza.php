5a863b4c48cbd2d551c4d19b7b434fbfb00b3a3e
André Marques <andre.m.d.abreu@gmail.com>
2023-10-17 08:53:43 -0300

    Update usuario-atualiza.php

--- admin/usuario-atualiza.php
+++ admin/usuario-atualiza.php
@@ -45,25 +45,27 @@ if(isset($_POST['atualizar'])){
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input class="form-control" type="text" id="nome" name="nome" required>
+				<input class="form-control" type="text" id="nome" name="nome" value="<?=$dadosDoUsuario['nome']?>" required>
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="email">E-mail:</label>
-				<input class="form-control" type="email" id="email" name="email" required>
+				<input class="form-control" type="email" id="email" name="email" value="<?=$dadosDoUsuario['email']?>" required>
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="senha">Senha:</label>
+
 				<input class="form-control" type="password" id="senha" name="senha" placeholder="Preencha apenas se for alterar">
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="tipo">Tipo:</label>
+
 				<select class="form-select" name="tipo" id="tipo" required>
 					<option value=""></option>
-					<option value="editor">Editor</option>
-					<option value="admin">Administrador</option>
+					<option <?php if($dadosDoUsuario['tipo'] === 'editor') echo ' selected '?> value="editor">Editor</option>
+					<option <?php if($dadosDoUsuario['tipo'] === 'admin') echo ' selected '?> value="admin">Administrador</option>
 				</select>
 			</div>
 			
