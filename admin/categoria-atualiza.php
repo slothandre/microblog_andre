75281f313243e6dc46d1b7ebc3c78e49cdf11c57
André Marques <andre.m.d.abreu@gmail.com>
2023-10-18 11:50:14 -0300

    Programação para atualizar uma categoria

--- admin/categoria-atualiza.php
+++ admin/categoria-atualiza.php
@@ -1,5 +1,17 @@
 <?php 
+use Microblog\Categoria;
 require_once "../inc/cabecalho-admin.php";
+$sessao->verificaAcessoAdmin();
+
+$categoria = new Categoria;
+$categoria->setId($_GET['id']);
+$dadosCategoria = $categoria->lerUm();
+
+if(isset($_POST['atualizar'])){
+	$categoria->setNome($_POST['nome']);
+	$categoria->atualizar();
+	header("location:categorias.php");
+}
 ?>
 
 
@@ -14,7 +26,7 @@ require_once "../inc/cabecalho-admin.php";
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input class="form-control" type="text" id="nome" name="nome" required>
+				<input class="form-control" type="text" id="nome" name="nome" value="<?=$dadosCategoria['nome']?>" required>
 			</div>
 			
 			<button class="btn btn-primary" name="atualizar"><i class="bi bi-arrow-clockwise"></i> Atualizar</button>
