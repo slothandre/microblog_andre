329bf6c8b3965e8def009f1da43556e7530362f5
André Marques <andre.m.d.abreu@gmail.com>
2023-10-18 11:51:51 -0300

    Programação para excluir uma categoria

--- admin/categoria-exclui.php
+++ admin/categoria-exclui.php
@@ -1,6 +1,13 @@
 <?php
-use Microblog\{ControleDeAcesso};
+use Microblog\{ControleDeAcesso, Categoria};
 require_once "../vendor/autoload.php";
 
 $sessao = new ControleDeAcesso;
 $sessao->verificaAcesso();
+$sessao->verificaAcessoAdmin();
+
+$categoria = new Categoria;
+$categoria->setId($_GET['id']);
+$categoria->excluir();
+
+header("location:categorias.php");
