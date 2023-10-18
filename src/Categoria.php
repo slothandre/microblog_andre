fdacd7197a192d60136419aba0108c63446595c4
André Marques <andre.m.d.abreu@gmail.com>
2023-10-18 11:15:47 -0300

    Criação da classe Categoria + parâmetros, conexão com o banco e getteres e setteres

---
+++ src/Categoria.php
@@ -0,0 +1,56 @@
+<?php
+namespace Microblog;
+use PDO, Exception;
+
+class Categoria {
+    private int $id;
+    private string $nome;
+    private PDO $conexao;
+
+    public function __construct() {
+        $this->conexao = Banco::conecta();
+    }
+
+    public function ler():array {
+        $sql = "SELECT * FROM categorias ORDER BY nome";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->execute();
+            $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+        } catch (Exception $erro) {
+            die("Erro ao exibir: ".$erro->getMessage());
+        }
+        return $resultado;
+    }
+
+    public function inserir():void {
+        $sql = "INSERT INTO categorias(nome) VALUES(:nome)";
+
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindValue(":nome", $this->nome, PDO::PARAM_STR);
+            $consulta->execute();
+        } catch (Exception $erro) {
+            die("Erro ao inserir: ".$erro->getMessage());
+        }
+    }
+
+    public function getId(): int{
+        return $this->id;
+    }
+    public function setId(int $id): self{
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNome(): string{
+        return $this->nome;
+    }
+    public function setNome(string $nome): self{
+        $this->nome = $nome;
+
+        return $this;
+    }
+}
