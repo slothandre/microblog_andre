5f063661adb52edf5caa7a782444d93ddde98968
André Marques <andre.m.d.abreu@gmail.com>
2023-10-24 08:44:15 -0300

    Criação da classe Noticia + propriedades + getters e setters

---
+++ src/Noticia.php
@@ -0,0 +1,91 @@
+<?php
+namespace Microblog;
+use PDO, Exception;
+
+final class Noticia {
+    private int $id;
+    private string $data;
+    private string $titulo;
+    private string $texto;
+    private string $resumo;
+    private string $imagem;
+    private string $destaque;
+    private string $termo; // será usado na busca
+    private PDO $conexao;
+
+    public function __construct() {
+        $this->conexao = Banco::conecta();
+    }
+
+    public function getId(): int {
+        return $this->id;
+    }
+    public function setId(int $id): self {
+        $this->id = filter_var($id, FILTER_SANITIZE_NUMBER_INT);
+
+        return $this;
+    }
+
+    public function getData(): string {
+        return $this->data;
+    }
+    public function setData(string $data): self {
+        $this->data = filter_var($data, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        return $this;
+    }
+
+    public function getTitulo(): string {
+        return $this->titulo;
+    }
+    public function setTitulo(string $titulo): self {
+        $this->titulo = filter_var($titulo, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        return $this;
+    }
+
+    public function getTexto(): string {
+        return $this->texto;
+    }
+    public function setTexto(string $texto): self {
+        $this->texto = filter_var($texto, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        return $this;
+    }
+
+    public function getResumo(): string {
+        return $this->resumo;
+    }
+    public function setResumo(string $resumo): self {
+        $this->resumo = filter_var($resumo, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        return $this;
+    }
+
+    public function getImagem(): string {
+        return $this->imagem;
+    }
+    public function setImagem(string $imagem): self {
+        $this->imagem = filter_var($imagem, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        return $this;
+    }
+
+    public function getDestaque(): string {
+        return $this->destaque;
+    }
+    public function setDestaque(string $destaque): self {
+        $this->destaque = filter_var($destaque, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        return $this;
+    }
+
+    public function getTermo(): string {
+        return $this->termo;
+    }
+    public function setTermo(string $termo): self {
+        $this->termo = filter_var($termo, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        return $this;
+    }
+}
