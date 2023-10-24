098a5373092399e5f9cd8d286a412b64c1431ac8
André Marques <andre.m.d.abreu@gmail.com>
2023-10-24 09:08:35 -0300

    Adicionando propriedades do tipo objeto usando classes já existentes, e no método construct criamos os objetos

--- admin/noticia-insere.php
+++ admin/noticia-insere.php
@@ -1,5 +1,8 @@
 <?php 
 require_once "../inc/cabecalho-admin.php";
+use Microblog\{Noticia, Utilitarios};
+$noticia = new Noticia;
+Utilitarios::dump($noticia);
 ?>
 
 
--- src/Noticia.php
+++ src/Noticia.php
@@ -13,7 +13,18 @@ final class Noticia {
     private string $termo; // será usado na busca
     private PDO $conexao;
 
+    /* Propriedades cujo tipo são ASSOCIADOS
+    à classes já existentes. Isso permitirá usar
+    recursos destas classes à partir de Noticia. */
+    public Usuario $usuario;
+    public Categoria $categoria;
+
     public function __construct() {
+        /* Ao criar um objeto Noticia, aproveitamos para
+        instanciar objetos de Usuario e Categoria */
+        $this->usuario = new Usuario;
+        $this->categoria = new Categoria;
+        
         $this->conexao = Banco::conecta();
     }
 
