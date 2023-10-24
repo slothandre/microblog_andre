4a9f320578e73469733ae7da24fb6de33c22188b
André Marques <andre.m.d.abreu@gmail.com>
2023-10-24 09:29:20 -0300

    Atualizações na conexão com o banco de dados para conectar somente uma vez, não importando quantas vezes é chamado o método

--- src/Banco.php
+++ src/Banco.php
@@ -7,20 +7,33 @@ abstract class Banco {
     private static string $usuario = "root";
     private static string $senha = "";
     private static string $banco = "microblog_andre";
-    private static PDO $conexao; 
+
+    /* Operador ? "nullable typehint"
+    Quando usado, indica que a propriedade/atributo
+    da classe pode conter um valor null OU pode ser
+    um o tipo PDO.
+    
+    Neste caso, a propriedade conexao é inicializada como
+    nula, mas a partir do momento em que uma conexão é feita
+    ela passa a valer PDO. */
+    private static ?PDO $conexao = null;
 
     public static function conecta():PDO {
-        try {
-            self::$conexao = new PDO(
-                "mysql:host=".self::$servidor."; 
-                dbname=".self::$banco.";
-                charset=utf8",
-                self::$usuario, 
-                self::$senha
-            );
-            self::$conexao->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (Exception $erro) {
-            die("Deu ruim: ".$erro->getMessage());
+        /* Só conecte se não houver conexão...
+        Se conexão for nula, faça as ações do try/catch */
+        if(self::$conexao === null) {
+            try {
+                self::$conexao = new PDO(
+                    "mysql:host=".self::$servidor."; 
+                    dbname=".self::$banco.";
+                    charset=utf8",
+                    self::$usuario, 
+                    self::$senha
+                );
+                self::$conexao->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (Exception $erro) {
+                die("Deu ruim: ".$erro->getMessage());
+            }
         }
         return self::$conexao;
     }
