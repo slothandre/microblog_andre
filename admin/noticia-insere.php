979dc0bcc251c4620efa69f385166491df29d7ff
André Marques <andre.m.d.abreu@gmail.com>
2023-10-24 11:02:19 -0300

    Configuração da inserção de dados + ajustes de formulário

--- admin/noticia-insere.php
+++ admin/noticia-insere.php
@@ -1,9 +1,26 @@
 <?php 
 require_once "../inc/cabecalho-admin.php";
-use Microblog\{Noticia, Utilitarios, Categoria};
+use Microblog\{Noticia, Utilitarios};
 
 $noticia = new Noticia;
 $listaDeCategorias = $noticia->categoria->ler();
+
+if(isset($_POST['inserir'])) {
+	$noticia->setTitulo($_POST['titulo']);
+	$noticia->setTexto($_POST['texto']);
+	$noticia->setResumo($_POST['resumo']);
+	$noticia->setDestaque($_POST['destaque']);
+
+	// ID do usuário que está inserindo a notícia
+	$noticia->usuario->setId($_SESSION['id']);
+
+	// ID da categoria escolhida para a notícia
+	$noticia->categoria->setId($_POST['categoria']);
+
+	/* Sobre a imagem
+	- Capturar o arquivo de imagem e enviar para o servidor
+	- Capturar o nome/extensão e enviar para o banco de dados */
+}
 ?>
 
 
@@ -14,8 +31,9 @@ $listaDeCategorias = $noticia->categoria->ler();
 		Inserir nova notícia
 		</h2>
 				
-		<form class="mx-auto w-75" action="" method="post" id="form-inserir" name="form-inserir">
-
+		<!-- Para que o formulário aceite arquivos (upload),
+		é necessário habilitar o atributo enctype -->
+		<form class="mx-auto w-75" action="" method="post" id="form-inserir" name="form-inserir" enctype="multipart/form-data">
             <div class="mb-3">
                 <label class="form-label" for="categoria">Categoria:</label>
                 <select class="form-select" name="categoria" id="categoria" required>
@@ -46,6 +64,7 @@ $listaDeCategorias = $noticia->categoria->ler();
                 <label class="form-label" for="imagem" class="form-label">Selecione uma imagem:</label>
                 <input required class="form-control" type="file" id="imagem" name="imagem"
                 accept="image/png, image/jpeg, image/gif, image/svg+xml">
+				<!-- Mime-type -->
 			</div>
 			
             <div class="mb-3">
