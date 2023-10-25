37f3f380e15b499432cf005ec06c1c5bcd87f9ec
André Marques <andre.m.d.abreu@gmail.com>
2023-10-25 10:00:38 -0300

    Refatoração para funcionar as notícias para o admin E editor

--- src/Noticia.php
+++ src/Noticia.php
@@ -90,7 +90,10 @@ final class Noticia {
 
         try {
             $consulta = $this->conexao->prepare($sql);
-            $consulta->bindValue(":usuario_id", $this->usuario->getId(), PDO::PARAM_INT);
+
+            /* Somente se NÃO for um admin, trate o parâmetro abaixo */
+            if($this->usuario->getTipo() !== "admin") $consulta->bindValue(":usuario_id", $this->usuario->getId(), PDO::PARAM_INT);
+            
             $consulta->execute();
             $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
         } catch (Exception $erro) {
