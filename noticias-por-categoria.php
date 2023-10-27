a5028fae251f6acf56b374ceded5f3feaf912724
André Marques <andre.m.d.abreu@gmail.com>
2023-10-27 09:45:21 -0300

    Update noticias-por-categoria.php

--- noticias-por-categoria.php
+++ noticias-por-categoria.php
@@ -8,31 +8,25 @@ $dados = $noticia->listarPorCategoria();
 <div class="row my-1 mx-md-n1">
 
     <article class="col-12">
-        <h2 class=" ">Notícias sobre <span class="badge bg-primary">categoria</span> </h2>
-        
-        <div class="row my-1">
+        <?php if(count($dados) > 0){ ?>
+            <h2 class="text-center">Notícias sobre <span class="badge bg-primary"><?=$dados[0]['categoria']?></span> </h2>
+        <?php } else { ?>
+            <h2 class="alert alert-warning text-center">Não há notícias desta categoria</h2>
+        <?php } ?>
+            <div class="row my-1">
             <div class="col-12 px-md-1">
                 <div class="list-group">
-                    <a href="noticia.php" class="list-group-item list-group-item-action">
-                        <h3 class="fs-6">Título da notícia</h3>
-                        <p><time>12/12/2012</time> - Autor da notícia</p>
-                        <p>Resumo da notícia</p>
-                    </a>
-                    <a href="noticia.php" class="list-group-item list-group-item-action">
-                        <h3 class="fs-6">Título da notícia</h3>
-                        <p><time>12/12/2012</time> - Autor da notícia</p>
-                        <p>Resumo da notícia</p>
-                    </a>
-                    <a href="noticia.php" class="list-group-item list-group-item-action">
-                        <h3 class="fs-6">Título da notícia</h3>
-                        <p><time>12/12/2012</time> - Autor da notícia</p>
-                        <p>Resumo da notícia</p>
-                    </a>
+                    <?php foreach($dados as $itemNoticia){ ?>
+                        <a href="noticia.php?id=<?=$itemNoticia['id']?>" class="list-group-item list-group-item-action">
+                            <h3 class="fs-6"><?=$itemNoticia['titulo']?></h3>
+                            <p><time><?=$itemNoticia['data']?></time> - <?=$itemNoticia['autor']?></p>
+                            <p><?=$itemNoticia['resumo']?></p>
+                        </a>
+                    <?php } ?>
                     
                 </div>
             </div>
         </div>
-
 
     </article>
     
